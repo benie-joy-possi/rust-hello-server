66a61a36999470b5f5fd7fd542dc89def6bb6781
benie-joy-possi <beniejoypossi@gmail.com>
2025-03-12 04:22:56 +0100

    updated index.html to create a file upload form to let us drop our files to the server and implemented a receiver for it in the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, extract::Multipart, response::Html, routing::get};
+
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../public/index.html"))
+}
+
+async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .expect("Failed to get next field!")
+    {
+        if field.name().unwrap() != "fileupload" {
+            continue;
+        }
+        println!("Got the file!")
+    }
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(index));
+    let app = Router::new().route("/", get(index).post(upload));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
@@ -11,8 +28,4 @@ async fn main() {
     axum::serve(listener, app)
         .await
         .expect("Failed to serve 'app'!"); // This function starts the HTTP server.
-}
-
-async fn index() -> Html<&'static str> {
-    Html(std::include_str!("../public/index.html"))
 }
