26dfb80bdbb543fb462aeb577f31aa6cc7ca55b3
benie-joy-possi <beniejoypossi@gmail.com>
2025-03-12 03:46:31 +0100

    Modified  the main.rs to print an errror message if failed to  start listening to server or failed to serve 'app'

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use axum::{Router, routing::get};
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to start Listener!");
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to serve 'app'!"); // This function starts the HTTP server.
 }
